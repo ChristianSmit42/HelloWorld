c1f526d86863b5f43b0a3b55efa57b764d57acf4
Christian Smit <chrsmit@web.de>
2020-05-12 17:28:37 +0200

    Add Methods

--- src/AppMain.java
+++ src/AppMain.java
@@ -2,7 +2,25 @@ public class AppMain {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+        int i = multi(8,9);
+        System.out.println("Summe: " + i);
+        compare(10,9);
     }
 
+    public static int multi(int a, int b){
+        return a*b;
+    }
+
+    public static void compare(int a, int b){
+        if(a>b){
+            System.out.println(a + " ist größer als " + b);
+        }
+        else if(a==b){
+            System.out.println(a + " ist genauso groß wie " + b);
+        }
+        else{
+            System.out.println(a + " ist kleiner als " + b);
+        }
+    }
 
 }
