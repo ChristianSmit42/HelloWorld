e91de31108b69dc9f296d41691fc8d37813bdbfb
Christian Smit <chrsmit@web.de>
2020-05-12 17:14:48 +0200

    Cleaned Code

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,10 +1,8 @@
 public class AppMain {
+
     public static void main(String[] args){
         System.out.println("Hello World");
-        multi(9,6);
     }
 
-    private int multi(int a, int b){
-        return a * b;
-    }
+
 }
