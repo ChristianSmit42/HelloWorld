3c199f60821cb34fef2485a860a6d5c8ebf96b6e
Christian Smit <chrsmit@web.de>
2020-05-12 16:59:19 +0200

    Add Method multi

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,5 +1,10 @@
 public class AppMain {
     public static void main(String[] args){
         System.out.println("Hello World");
+        multi(9,6);
+    }
+
+    private int multi(int a, int b){
+        return a * b;
     }
 }
